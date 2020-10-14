71880a7cb31d54f194462549bec3bd3113dd5ce6
Martin PELCAT <martinpelcat@hotmail.com>
2020-10-14 21:33:38 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex">
+      <div className="m-auto animate-bounce">
+        <span className="text-7xl tracking-widest font-bold capitalize">
+          Hello
+        </span>
+      </div>
+    </div>
+  );
+}
